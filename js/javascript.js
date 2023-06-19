ff61f68f054cee7b3f833942941f4e0b7dce7c9e
Hager Ramadan <78318946+hageramadan@users.noreply.github.com>
2023-06-19 04:47:17 +0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,45 @@
+// var inputEl=document.querySelector("input");
+// var btnEl=document.getElementById("change");
+// var see =document.getElementById("see");
+// var not =document.getElementById("not");
+// function show(){
+//     if(inputEl.type=="password"){
+//     inputEl.type="text";
+//     see.style.display='inline';
+//     not.style.display='none';
+    
+//        }
+//        else if(inputEl.type=="text"){
+//         inputEl.type="password";
+//         not.style.display='inline';
+//         see.style.display='none';
+//            }
+//            console.log(see.style.display);
+// }
+// btnEl.addEventListener('click',show)
+// 
+//  $(".d-show").click(()=>{
+//     $(".show-section").toggle(3000)
+//  },5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
